369620691a93d9ebabdf134aaf9125e5daa361b9
kayo <kaytoarcealcaraz@gmail.com>
2022-10-05 22:37:18 -0300

    First Commit

---
+++ presentation.js
@@ -0,0 +1,44 @@
+const presentationRow = document.querySelector(".presentation__row");
+console.log(presentationRow);
+const presentationImg = document.querySelector(".presentation__img");
+const presentationText = document.querySelector(".presentation__text");
+
+const setLayout = () => {
+  if (window.innerWidth < 768) {
+    presentationImg.classList.add("order-first");
+    presentationImg.classList.remove("img-md-width");
+
+    presentationText.classList.add("order-last");
+    presentationText.classList.remove("text-md-width");
+
+    presentationRow.classList.remove("row-md-justify");
+  } else {
+    presentationImg.classList.remove("order-first");
+    presentationImg.classList.add("img-md-width");
+
+    presentationText.classList.remove("order-last");
+    presentationText.classList.add("text-md-width");
+
+    presentationRow.classList.add("row-md-justify");
+  }
+};
+
+setLayout()
+
+window.addEventListener("resize",() => {
+  if (window.innerWidth < 768) {
+    presentationImg.classList.add("order-first");
+    presentationImg.classList.remove("img-md-width");
+    presentationText.classList.remove("text-md-width");
+    presentationText.classList.add("order-last");
+    presentationRow.classList.remove("row-md-justify");
+
+  } else{
+      presentationImg.classList.remove("order-first");
+      presentationImg.classList.add("img-md-width");
+      presentationText.classList.remove("order-last");
+      presentationText.classList.add("text-md-width");
+      presentationRow.classList.add("row-md-justify");
+  }
+})
+
